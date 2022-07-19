f504298aeebe86d64aef98efbd8d0a97066486aa
RomanYakobchuk <romanyakobchuk28@gmail.com>
2022-07-19 18:17:29 +0300

    part5 19.07.22 class vs function component lifecycles

--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
-import {useReducer} from "react";
+import {FunctionComponent} from "./components";
+import {useState} from "react";
 
-
-const init = (initCount) => {  // initCount - initializerArg
-    return {count1: initCount, count2: initCount} //Object - state
-}
-
-const reducer = (state, action)=> { // action - object in dispatch
-    switch (action.type) {
-        case 'inc':
-            return {...state, count1: state.count1+1}
-        case 'dec':
-            return {...state, count1: state.count1-1}
-        case 'res':
-            return {...state, count1: 0}
-        case 'set':
-            return {...state, count1: action.payload}
-    }
-}
 
 const App = () => {
     
-    const [state, dispatch] = useReducer(reducer, 0, init);
+    const [flag, setFlag] = useState(true);
 
-    // const inc = () => {
-    //     dispatch({type: 'inc'})
-    // }
-    
+
+
     return (
         <div>
             <div>
-                {state.count1}
-                <button onClick={() => dispatch({type: 'inc'})}>inc</button>
-                <button onClick={() => dispatch({type: 'dec'})}>dec</button>
-                <button onClick={() => dispatch({type: 'res'})}>reset</button>
-                <button onClick={() => dispatch({type: 'set', payload: 10})}>setTo10</button>
+                {flag && <FunctionComponent ssss={'MyProps'}/>}
             </div>
+            <button onClick={() => setFlag(prev => !prev)}>Hide</button>
         </div>
     );
 };
