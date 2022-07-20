30d9db70d1b1ae4fa3b7fdd594bb0021b34abe3d
RomanYakobchuk <romanyakobchuk28@gmail.com>
2022-07-20 19:41:43 +0300

    part5 19.07.22 class vs function component lifecycles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,63 @@
-import {FunctionComponent} from "./components";
-import {useState} from "react";
+import {useReducer} from "react";
 
+
+const reducer = (state, action) => {
+
+    const {type, payload} = action;
+    // console.log(type)
+
+    switch (type) {
+        case 'counter1':
+            return {...state, counter1: state.counter1 + payload}
+        case 'counter2':
+            return {...state, counter2: state.counter2 + payload}
+        case 'counter3':
+            return {...state, counter3: state.counter3 + payload}
+
+        // case 'dec1':
+        //     return {...state, counter1: state.counter1-1}
+        // case 'dec2':
+        //     return {...state, counter2: state.counter2-1}
+        // case 'dec3':
+        //     return {...state, counter3: state.counter3-1}
+
+        case 'reset':
+            const key = 'counter' + payload;
+            console.log(key)
+            // return {...state, counter+`${payload}`: 0}
+
+
+        default:
+            return state
+    }
+}
 
 const App = () => {
-    
-    const [flag, setFlag] = useState(true);
 
-
+    const [state, dispatch] = useReducer(reducer, {counter1: 0, counter2: 0, counter3: 0});
 
     return (
         <div>
             <div>
-                {flag && <FunctionComponent ssss={'MyProps'}/>}
+                <div>Count #1 : {state.counter1}</div>
+                <button onClick={() => dispatch({type: 'counter1', payload: 1})}>Inc</button>
+                <button onClick={() => dispatch({type: 'counter1', payload: -1})}>Dec</button>
+                <button onClick={() => dispatch({type: 'reset', payload: 1})}>Res</button>
             </div>
-            <button onClick={() => setFlag(prev => !prev)}>Hide</button>
+            <hr/>
+            <div>
+                <div>Count #2 : {state.counter2}</div>
+                <button onClick={() => dispatch({type: 'counter2', payload: 1})}>Inc</button>
+                <button onClick={() => dispatch({type: 'counter2', payload: -1})}>Dec</button>
+                <button onClick={() => dispatch({type: 'reset', payload: 2})}>Res</button>
+            </div>
+            <hr/>
+            <div>
+                <div>Count #3 : {state.counter3}</div>
+                <button onClick={() => dispatch({type: 'counter3', payload: 1})}>Inc</button>
+                <button onClick={() => dispatch({type: 'counter3', payload: -1})}>Dec</button>
+                <button onClick={() => dispatch({type: 'reset', payload: 3})}>Res</button>
+            </div>
         </div>
     );
 };
--- src/components/index.js
+++
@@ -1,2 +0,0 @@
-export * from './ClassComponent'
-export * from './FunctionComponent'
