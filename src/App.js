494a5e201a18e7fa6d30684c14cc4a9fa765ee9a
RomanYakobchuk <romanyakobchuk28@gmail.com>
2022-08-04 14:11:19 +0300

    7_1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import './App.css';
-import {useState} from "react";
+// import './App.css';
 
-const reducer = (state, action) => {
-
-  const {type, payload} = action
-
-  switch (type) {
-    case 'addCat':
-      return {...state, cats: [...state.cats, {name: payload, id: Date.now()}]}
-    case "deleteCat":
-      return {...state, cats: state.cats.filter(cat => cat.id !== payload)}
-  }
-}
 
 
 function App() {
 
-  const [catValue, setCatValue] = useState('');
-  const [dogValue, setDogValue] = useState('');
+
   return (
     <div >
 
